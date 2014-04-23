d59730692c03d03c633911c774c1f57ce611461b
mcarthurgill <mickgill2008@gmail.com>
2014-04-23 13:54:43 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityRails::Application.config.secret_token = 'c6bd94d013ffe8841bd49f23de7fcde2ed4b4a12618e0e508102723e94f1b2f2befda69989761412247524e95b9c7af3a0b75920994d3352e099f0dcb1a87181'
