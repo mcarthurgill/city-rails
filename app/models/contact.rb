a7a06185460b39f8d3d4b03bdc530f96132b875e
Billy <william@busproductions.com>
2014-04-29 13:36:48 -0500

    fix rollback

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   attr_accessible :name, :phone_number, :user_id
 
-  validates_presence_of :name, :phone_number, :user_id
+  validates_presence_of :phone_number, :user_id
 
   belongs_to :user
   belongs_to :connection, :class_name => 'User', :foreign_key => 'phone_number', :primary_key => 'phone'
