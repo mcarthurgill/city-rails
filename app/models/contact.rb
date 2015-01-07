2f632fcc137934c99773795e6af4a4cb28e8e432
mcarthurgill <mickgill2008@gmail.com>
2015-01-07 12:56:59 -0600

    block fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,4 @@ class Contact < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :connection, :class_name => 'User', :foreign_key => 'phone_number', :primary_key => 'phone'
-
-  scope :unblocked, ->{ where('blocked = ?', false) }
 end
