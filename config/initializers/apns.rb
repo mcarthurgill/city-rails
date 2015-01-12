9a5049371863ae1166ca17be1ec745503ab8c05e
w <w@lxv.io>
2015-01-12 10:34:40 -0600

    meow

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,3 +1,3 @@
-APNS.host = "gateway.sandbox.push.apple.com"
-APNS.pem = File.join(Rails.root, "ZroundDevelopment.pem")
-APNS.port = 2195 
+# APNS.host = "gateway.sandbox.push.apple.com"
+# APNS.pem = File.join(Rails.root, "ZroundDevelopment.pem")
+# APNS.port = 2195 
